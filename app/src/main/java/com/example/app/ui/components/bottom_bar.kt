8e098f57c5c082b9fb0d55d75b1af7f08f501e17
kweonminsung <kevin136583@gmail.com>
2025-07-04 22:15:51 +0900

    chore: rename searchbar

--- app/src/main/java/com/example/app/ui/components/bottom_bar.kt
+++ app/src/main/java/com/example/app/ui/components/bottom_bar.kt
@@ -31,25 +31,25 @@ fun BottomBar(
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        BottomBarButton("홈", Icons.Default.Home) { onItemClick("home") }
-        BottomBarButton("사진", Icons.Default.Phone) { onItemClick("photos") }
-        BottomBarButton("지도", Icons.Default.Add) { onItemClick("add") }
-        BottomBarButton("프로필", Icons.Default.Person) { onItemClick("profile") }
+//        BottomBarButton("홈", Icons.Default.Home) { onItemClick("home") }
+//        BottomBarButton("사진", Icons.Default.Phone) { onItemClick("photos") }
+//        BottomBarButton("지도", Icons.Default.Add) { onItemClick("add") }
+//        BottomBarButton("프로필", Icons.Default.Person) { onItemClick("profile") }
     }
 }
 
-@Composable
-fun IconButtonExample() {
-    Button(
-        onClick = { /* 버튼 클릭 시 동작 */ },
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFEEEEEE))
-    ) {
-        Icon(
-            imageVector = ImageVector.vectorResource(id = app.src.res.drawable.ic_home),
-            contentDescription = "홈 아이콘",
-            modifier = Modifier.size(20.dp)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Text("홈")
-    }
-}
+//@Composable
+//fun IconButtonExample() {
+//    Button(
+//        onClick = { /* 버튼 클릭 시 동작 */ },
+//        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFEEEEEE))
+//    ) {
+//        Icon(
+//            imageVector = ImageVector.vectorResource(id = app.src.res.drawable.ic_home),
+//            contentDescription = "홈 아이콘",
+//            modifier = Modifier.size(20.dp)
+//        )
+//        Spacer(modifier = Modifier.width(8.dp))
+//        Text("홈")
+//    }
+//}
