bda4279a772e3388a37d0e045bf12d18f7447135
jien040708 <jien0478@kaist.ac.kr>
2025-07-04 22:25:29 +0900

    fix: error

--- app/src/main/java/com/example/app/ui/components/bottom_bar.kt
+++ app/src/main/java/com/example/app/ui/components/bottom_bar.kt
@@ -1,5 +1,6 @@
 package com.example.app.ui.components
 
+import com.example.app.R
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -14,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
