d6c5af7af7218dcd0d1d1e64cdeb8e9aaf8fe889
kweonminsung <kevin136583@gmail.com>
2025-07-04 21:34:55 +0900

    feat: add tab components for Home, Album, Map, and Profile

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import com.example.app.ui.theme.AppTheme
 import com.example.app.ui.components.Header
+import com.example.app.ui.pages.AlbumTab
+import com.example.app.ui.pages.HomeTab
+import com.example.app.ui.pages.MapTab
+import com.example.app.ui.pages.ProfileTab
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,12 +34,24 @@ enum class TabType {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    var tabType: TabType = TabType.HOME
+    var tabType by remember { mutableStateOf(TabType.HOME) }
 
     Column(
         modifier = modifier.fillMaxSize()
     ) {
-        Header(tabType, )
+        Row {
+            Button(onClick = { tabType = TabType.HOME }) { Text("홈") }
+            Button(onClick = { tabType = TabType.ALBUM }) { Text("앨범") }
+            Button(onClick = { tabType = TabType.MAP }) { Text("지도") }
+            Button(onClick = { tabType = TabType.PROFILE }) { Text("프로필") }
+        }
+        Header(tabType)
 
+        when (tabType) {
+            TabType.HOME -> HomeTab()
+            TabType.ALBUM -> AlbumTab()
+            TabType.MAP -> MapTab()
+            TabType.PROFILE -> ProfileTab()
+        }
     }
 }
---
+++ app/src/main/java/com/example/app/ui/pages/profile.kt
@@ -0,0 +1,11 @@
+package com.example.app.ui.pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileTab() {
+    Text(
+        text = "Profile Tab",
+    )
+}
