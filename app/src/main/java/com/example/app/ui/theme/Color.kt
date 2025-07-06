9e48bea4b7f2abffd3c4b063ae348c49495a5606
kweonminsung <kevin136583@gmail.com>
2025-07-06 22:24:43 +0900

    fix: blue color 병합

--- app/src/main/java/com/example/app/ui/theme/Color.kt
+++ app/src/main/java/com/example/app/ui/theme/Color.kt
@@ -12,7 +12,6 @@ object CustomColors {
 
     val Blue = Color(0xFF4696FF)
     val Transparent = Color(0x00000000)
-    val Blue = Color(0xFF1E85E5)
     val Green = Color(0xFF4CAF50)
     val Red = Color(0xFFF44336)
     val Yellow = Color(0xFFF9E000)
