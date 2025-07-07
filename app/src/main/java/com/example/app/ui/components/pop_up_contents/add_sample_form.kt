b318638c655637e0b285c23528d6d013eb613c56
jien040708 <jien0478@kaist.ac.kr>
2025-07-07 18:14:24 +0900

    feat: 팝업창 샘플 구현 중

--- app/src/main/java/com/example/app/ui/components/pop_up_contents/add_sample_form.kt
+++ app/src/main/java/com/example/app/ui/components/pop_up_contents/add_sample_form.kt
@@ -1,13 +1,23 @@
 package com.example.app.ui.components.pop_up_contents
 
+import androidx.compose.foundation.background
+import com.example.app.R
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -16,12 +26,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.app.ui.components.PopupWindow
 import com.example.app.ui.theme.CustomColors
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
+import kotlin.Boolean
 
 
 //@Composable
@@ -49,7 +61,8 @@ fun AddSampleContent() {
         TextField(
             value = "",
             onValueChange = {},
-            label = { Text("이름을 입력하세요...", color = CustomColors.Gray) },
+            label = { Text("여행 이름") },
+            placeholder = { Text("이름을 입력하세요...", color = CustomColors.Gray) },
             colors = TextFieldDefaults.colors(
                 focusedContainerColor = CustomColors.LightGray,
                 unfocusedContainerColor = CustomColors.White,
@@ -64,22 +77,40 @@ fun AddSampleContent() {
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        // 위치 입력 필드
+        // 위치 보이는 필드
         TextField(
             value = "",
             onValueChange = {  },
             modifier = Modifier.weight(1f), // TextField가 남는 공간 채움
-            placeholder = { Text("입력하세요") }
+            placeholder = { Text("위치를 선택하세요...", color = CustomColors.Gray) },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = CustomColors.LightGray,
+                unfocusedContainerColor = CustomColors.White,
+                focusedIndicatorColor = CustomColors.Gray,
+                unfocusedIndicatorColor = CustomColors.DarkGray
+            ),
+            readOnly = true
         )
 
         Spacer(modifier = Modifier.width(8.dp)) // 여백
 
         // 지도에서 위치 선택 버튼
-        Button (onClick = {}) {
-            Text("지도에서 위치 선택")
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .height(45.dp)
+                .width(45.dp)
+                .background(
+                    color = CustomColors.White,
+                    shape = RoundedCornerShape(4.dp)
+                )
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.map_etc),
+                contentDescription = "추가"
+            )
         }
-}
-
+    }
 
 }
 
